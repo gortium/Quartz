7cf7ce6b988e136f903b93b6b5baf501ef242db0
Thierry Pouplier <thierrypouplier@gmail.com>
2024-11-25 17:25:21 -0500

    Quartz sync: Nov 25, 2024, 5:25 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/gortium",
     },
   }),
 }
@@ -60,6 +59,11 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+  ],
+  afterBody: [
+    Component.RecentNotes({
+      showTags: false,
+    }),
   ],
 }
 
